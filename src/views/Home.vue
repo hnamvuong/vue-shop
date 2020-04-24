a7b8de1bedbad69624de866be429ac9b8cf6ce2c
hnam.vuong <hnam.vuong@gmail.com>
2020-04-24 10:32:28 +0700

    hero for home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
-    <div class="home container">
+    <div class="home">
+        <Hero/>
     </div>
 </template>
+
+<script>
+import Hero from '@/components/Hero.vue'
+
+export default {
+    name: 'home',
+    components: {
+        Hero
+    }    
+}
+</script>
 
 <style>
 

fd73538c155be4725d14af882ce86c380c2bda63
hnam.vuong <hnam.vuong@gmail.com>
2020-04-25 11:02:31 +0700

    update product via firebase

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -44,7 +44,7 @@
                             <td> {{ product.data().name }}</td>
                             <td> {{ product.data().price }}</td>
                             <td>
-                                <button class="btn btn-primary">Edit</button>
+                                <button @click="editProduct(product)" class="btn btn-primary">Edit</button>
                                 <button @click="deleteProduct(product.id)" class="btn btn-danger">Delete</button>
                             </td>
                         </tr>
@@ -53,13 +53,38 @@
                 </div>
             </div>
         </div>
+        <!-- Modal -->
+        <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="editLabel"
+             aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Edit Product</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" placeholder="Price" v-model="product.price" class="form-control">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button @click="updateProduct" type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    import {
-        db
-    } from '../firebase';
+    import {db} from '../firebase';
+    import $ from 'jquery';
 
     export default {
         name: "Products",
@@ -72,10 +97,36 @@
                 product: {
                     name: null,
                     price: null
-                }
+                },
+                activeItem: '',
             }
         },
         methods: {
+            watcher(){
+                db.collection("products").onSnapshot((querySnapshot) => {
+                    this.products = [];
+                    querySnapshot.forEach((doc) => {
+                        this.products.push(doc);
+                    });
+                });
+            },
+            updateProduct() {
+                db.collection("products").doc(this.activeItem).update(this.product)
+                    .then(() => {
+                        $('#edit').modal('hide');
+                        this.watcher();
+                        console.log("Document successfully updated!");
+                    })
+                    .catch(function(error) {
+                        // The document probably doesn't exist.
+                        console.error("Error updating document: ", error);
+                    });
+            },
+            editProduct(product) {
+                $('#edit').modal('show');
+                this.product = product.data();
+                this.activeItem = product.id;
+            },
             deleteProduct(doc) {
                 if (confirm('Are you sure?')) {
                     db.collection("products").doc(doc).delete().then(function () {
@@ -91,6 +142,7 @@
             readData() {
                 db.collection("products").get().then((querySnapshot) => {
                     // this.products = querySnapshot;
+                    this.products = [];
                     querySnapshot.forEach((doc) => {
                         // doc.data() is never undefined for query doc snapshots
                         this.products.push(doc);

f30777628a8d54b2dfb5b296ff95f078ccd9279d
hnam.vuong <hnam.vuong@gmail.com>
2020-04-25 09:35:39 +0700

    read product from firebase

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -14,26 +14,45 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <hr>
-        <h3>CRUD (create/read/update/delete) in Firebase and Vue</h3>
-        <div class="product-test">
-            <div class="form-group">
-                <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
-            </div>
-            <div class="form-group">
-                <input type="text" placeholder="Price" v-model="product.price" class="form-control">
-            </div>
-            <div class="form-group">
-                <button @click="saveData" class="btn btn-primary">Save Data</button>
+            <hr>
+            <h3>Basic CRUD (create/read/update/delete) in Firebase and Vue</h3>
+            <div class="product-test">
+                <div class="form-group">
+                    <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <input type="text" placeholder="Price" v-model="product.price" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <button @click="saveData" class="btn btn-primary">Save Data</button>
+                </div>
+                <hr>
+                <h3>Products list</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(product, index) in products" :key="index">
+                            <td> {{ product.name }} </td>
+                            <td> {{ product.price }} </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import {db} from '../firebase';
-
+    import {
+        db
+    } from '../firebase';
     export default {
         name: "Products",
         props: {
@@ -41,26 +60,39 @@
         },
         data() {
             return {
+                products: [],
                 product: {
-                    name: '',
-                    price: ''
-                },
+                    name: null,
+                    price: null
+                }
             }
         },
         methods: {
+            readData() {
+                db.collection("products").get().then((querySnapshot) => {
+                    // this.products = querySnapshot;
+                    querySnapshot.forEach((doc) => {
+                        // doc.data() is never undefined for query doc snapshots
+                        this.products.push(doc.data());
+                    });
+                });
+            },
             saveData() {
                 db.collection("products").add(this.product)
-                .then((docRef) => {
-                    console.log("Document written with ID: ", docRef.id);
-                    this.reset();
-                })
-                .catch(function(error) {
-                    console.error("Error adding document: ", error);
-                });
-                },
-                reset(){
-                Object.assign(this.$data, this.$options.data.apply(this));
+                    .then((docRef) => {
+                        console.log("Document written with ID: ", docRef.id);
+                        this.readData();
+                    })
+                    .catch(function (error) {
+                        console.error("Error adding document: ", error);
+                    });
+            },
+            reset() {
+                // Object.assign(this.$data, this.$options.data.apply(this));
             }
+        },
+        created() {
+            this.readData();
         }
     };
 </script>
